b0c2bfcad330695141025dd5f3b88832cab34e35
lihuate <lihuate@gmail.com>
2019-07-03 18:13:34 +0800

    Update opencv4.1.0-install.sh

--- opencv4.1.0-install.sh
+++ opencv4.1.0-install.sh
@@ -11,9 +11,9 @@ apt-get install -y c++ && apt-get install -y g++ && apt-get install -y gcc && ap
 apt-get install -y ncurses-devel && apt-get install -y automake 
 
 
-cd /home/
-git clone https://github.com/lihuate/opencv4.1.0-raspbian-install.git
-cp -R opencv4.1.0-raspbian-install/opencv4.1.0 /home/opencv4.1.0 && rm -rf opencv4.1.0-raspbian-install/
+cd /home/ && git clone https://github.com/lihuate/opencv4.1.0-raspbian-install.git
+rm -rf /home/opencv4.1.0 && cd /home/
+cp -R /home/opencv4.1.0-raspbian-install/opencv4.1.0 /home/opencv4.1.0 && rm -rf opencv4.1.0-raspbian-install/
 echo "/home/opencv/opencv4.1.0/lib" >/etc/ld.so.conf.d/opencv.conf
 ldconfig
 echo  'export PKG_CONFIG_PATH=/home/opencv4.1.0/lib/pkgconfig:$PKG_CONFIG_PATH'   >>  ~/.bashrc
