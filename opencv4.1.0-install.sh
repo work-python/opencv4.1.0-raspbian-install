84045189550572002bac0e9e4a24990288ef8de0
lihuate <lihuate@gmail.com>
2019-07-04 09:47:54 +0800

    Update opencv4.1.0-install.sh

--- opencv4.1.0-install.sh
+++ opencv4.1.0-install.sh
@@ -10,7 +10,7 @@ apt-get install -y libtiff-dev && apt-get install -y libjasper-dev
 apt-get install -y libdc1394-22-dev
 apt-get install -y c++ && apt-get install -y g++ && apt-get install -y gcc && apt-get install -y autoconf
 apt-get install -y ncurses-devel && apt-get install -y automake 
-#######################################################################################################################################
+#######
 if  [ -d "/home/opencv4.1.0/" ];then
   echo  "文件夹存在"
   rm -rf /home/opencv4.1.0 && cd /home/
@@ -21,7 +21,7 @@ else
   cd /home/ && git clone https://github.com/lihuate/opencv4.1.0-raspbian-install.git
   cp -R /home/opencv4.1.0-raspbian-install/opencv4.1.0 /home/opencv4.1.0 && rm -rf opencv4.1.0-raspbian-install/
 fi
-########################################################################################################################################
+#######
 if  [ -f  "/etc/ld.so.conf.d/opencv.conf" ];then
   echo  "/etc/ld.so.conf.d/opencv.conf文件存在"
 grep "/home/opencv/opencv4.1.0/lib" /etc/ld.so.conf.d/opencv.conf >/dev/null
@@ -36,7 +36,7 @@ else
   echo "/home/opencv/opencv4.1.0/lib" >/etc/ld.so.conf.d/opencv.conf
 fi
 ldconfig
-###############################
+##########
 #if  [ -f  '~/.bashrc' ];then
 #  echo  ".bashrc文件存在"
 #  else
