d3e74862ca65ae4b71d37321fd29bcca79326e94
lihuate <lihuate@gmail.com>
2019-07-04 16:13:35 +0800

    opencv4.1.0解压版

--- opencv4.1.0-install.sh
+++ opencv4.1.0-install.sh
@@ -75,27 +75,33 @@ if [ $? -ne 0 ]; then
     echo "失败，"
     apt-get install libcurl4-gnutls-dev
     echo "开始安装cmake3.10.2"
+#     wget https://raw.githubusercontent.com/lihuate/opencv4.1.0-raspbian-install/master/cmake-3.9.0.tar.gz
     wget https://raw.githubusercontent.com/lihuate/opencv4.1.0-raspbian-install/master/cmake-3.9.0.tar.gz
     tar -xzf cmake-3.9.0.tar.gz
     cd cmake-3.9.0/
-    ./bootstrap --system-curl
-    make -j2 && make install
+#     ./bootstrap --system-curl
+#     make -j2 && make install
     sudo apt remove cmake
     sudo make install
 else
-    echo "成功系统存在cmake"
+
+echo `cmake -version` > e.txt
+path=`cut -d ' ' -f 3 e.txt`
+rm -rf e.txt
+if [[ $path =~ "3.9.0" ]] ; then
+    echo "安装成功"
     echo "cmake版本及库信息："
-    echo "请确认当前版本支持c++11，"
-    echo opencv版本 `cmake -version`
+    echo cmake版本 $path
+else
+echo "失败，请手动cmake1.10.X以上版本"
 fi
-cmake -version
-if [ $? -ne 0 ]; then
-    echo "失败，请手动安装cmake1.10.X以上版本"
- else  
-fi 
+
+
 rm -rf cmake-3.10.2.tar.gz
 cd ..
 rm -rf cmake-3.10.2/
+
+
 pkg-config --modversion opencv4
 if [ $? -ne 0 ]; then
     echo "失败，自我删除"
