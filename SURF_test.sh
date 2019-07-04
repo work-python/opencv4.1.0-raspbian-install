3572050a0b0c39dd65e23a7b221ed2aae1a75d77
lihuate <lihuate@gmail.com>
2019-07-04 15:56:43 +0800

    SURF_test 特征点检测 opencv

--- SURF_test.sh
+++ SURF_test.sh
@@ -8,13 +8,13 @@ fi
 echo `cmake -version` > e.txt
 path=`cut -d ' ' -f 3 e.txt`
 rm -rf e.txt
-if [[ $path =~ "3.9.0.1" ]] ; then
+if [[ $path =~ "3.9.0" ]] ; then
    echo '3.9.0存在!'
 echo '程序继续执行!'
 else
     echo '找不到3.9.0'
     echo 当前cmake版本号是 $path
-    sed -i "s/3.10.2/$path/g" CMakeLists.txt
+    sed -i "s/3.9.0/$path/g" CMakeLists.txt
     echo 替换成功 ,继续运行
 fi
 
