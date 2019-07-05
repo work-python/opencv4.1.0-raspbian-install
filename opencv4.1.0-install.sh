388a274abc91637eeab79193375245284400b8a0
lihuate <lihuate@gmail.com>
2019-07-05 10:47:31 +0800

    Update opencv4.1.0-install.sh

--- opencv4.1.0-install.sh
+++ opencv4.1.0-install.sh
@@ -92,14 +92,8 @@ if [[ $path =~ "3.9.0" ]] ; then
     echo "安装成功"
     echo "cmake版本及库信息："
     echo cmake版本 $path
-else
-echo "失败，请手动cmake1.10.X以上版本"
+
 fi
-
-
-rm -rf cmake-3.10.2.tar.gz
-cd ..
-rm -rf cmake-3.10.2/
 
 
 pkg-config --modversion opencv4
